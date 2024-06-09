584fb5a345568c308f10e114f4fde68b39e5d669
SamManoli975 <C23330671@mytudublin.ie>
2024-06-09 14:06:19 +0100

    adding a map and hovering over the specific country showing the name

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import './App.css';
 
-function App() {
+function Map() {
   const [geoData, setGeoData] = useState(null);
+  const [tooltip, setTooltip] = useState({ show: false, x: 0, y: 0, name: '' });
 
   useEffect(() => {
     fetch('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json')
@@ -14,23 +16,52 @@ function App() {
 
   const onEachCountry = (country, layer) => {
     layer.on({
-      hover: () => {
-        alert(`Country: ${country.properties.name}`);
+      mouseover: (e) => {
+        setTooltip({
+          show: true,
+          x: e.originalEvent.clientX,
+          y: e.originalEvent.clientY,
+          name: country.properties.name,
+        });
+      },
+      mouseout: () => {
+        setTooltip({
+          show: false,
+          x: 0,
+          y: 0,
+          name: '',
+        });
+      },
+      mousemove: (e) => {
+        setTooltip((prev) => ({
+          ...prev,
+          x: e.originalEvent.clientX,
+          y: e.originalEvent.clientY,
+        }));
       }
     });
   };
 
   return (
-    <MapContainer center={[20, 0]} zoom={2} style={{ height: "100vh", width: "100%" }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      {geoData && (
-        <GeoJSON data={geoData} onEachFeature={onEachCountry} />
+    <div>
+      <MapContainer center={[20, 0]} zoom={2} style={{ height: "100vh", width: "100%" }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {geoData && (
+          <GeoJSON data={geoData} onEachFeature={onEachCountry} />
+        )}
+      </MapContainer>
+      {tooltip.show && (
+        <div className="tooltip" style={{ left: tooltip.x + 10, top: tooltip.y + 10 }}>
+          <strong>{tooltip.name}</strong>
+      
+
+        </div>
       )}
-    </MapContainer>
+    </div>
   );
 }
 
-export default App;
+export default Map;
