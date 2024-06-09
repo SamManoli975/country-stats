f516b2628df010dac9af2bac6e2ed75fd3b66366
SamManoli975 <C23330671@mytudublin.ie>
2024-06-09 13:36:07 +0100

    simple maps url didnt work so we are using react leaflet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
+  const [geoData, setGeoData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json')
+      .then(response => response.json())
+      .then(data => setGeoData(data))
+      .catch(error => console.error('Error loading GeoJSON:', error));
+  }, []);
+
+  const onEachCountry = (country, layer) => {
+    layer.on({
+      hover: () => {
+        alert(`Country: ${country.properties.name}`);
+      }
+    });
+  };
+
   return (
-    <div>
-      <h1>country stats</h1>
-    </div>
+    <MapContainer center={[20, 0]} zoom={2} style={{ height: "100vh", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {geoData && (
+        <GeoJSON data={geoData} onEachFeature={onEachCountry} />
+      )}
+    </MapContainer>
   );
 }
 
